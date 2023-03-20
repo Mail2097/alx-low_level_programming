7916c363743ed83934a00d01814d1505fb0c2125
Mail2097 <okoriejane97@gmail.com>
2023-03-20 15:01:25 -0700

    hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -prints the size if various types based
+ * on the computer it is complied and run on.
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sissof(d))$
+	printf("size of an int: %lu byte(s)\n, (unsigned long)sissof(a))$
+        printf("size of a long int: %lu byte(s)\n",(unsigned long)sissof(b))$
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sissof(c))$
+	printf("size of a float: %lu byte(s)\n", (unssigned lonf)sissof(e))$
+	  
