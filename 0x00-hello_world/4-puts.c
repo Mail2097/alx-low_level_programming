98ce28d95c9c98f62d4215395d15b014323f5b78
Mail2097 <okoriejane97@gmail.com>
2023-03-20 10:36:07 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzule");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
