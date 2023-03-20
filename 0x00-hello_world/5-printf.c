b71317456561572d74c8da340758792fb3a3e67a
Mail2097 <okoriejane97@gmail.com>
2023-03-20 12:22:52 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
-int main 0 {
-	printf("Behold, what a magnificent work of art this is!\n");
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
-
