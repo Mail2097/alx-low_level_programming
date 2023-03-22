caa4367f5ab5007772e56f39e69c1d84066c9437
Mail2097 <okoriejane97@gmail.com>
2023-03-22 05:12:12 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
+#include <stduo.h>
 #include <unistd.h>
 /**
- * main -write to std out
- * Return: always 0
+ * main - A c program that prints a line to the standard error
+ * Return: always 1 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return(1);
 }
