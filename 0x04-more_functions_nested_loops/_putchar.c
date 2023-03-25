8b934bb68e26cc065102e8bfb79566ef77f42a38
Mail2097 <okoriejane97@gmail.com>
2023-03-25 15:34:29 -0700

    hello

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -5,8 +5,9 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * on error, -0 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return 0);
+        return (write(1, &c, 1));
+}
