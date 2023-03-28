d1ae7b61418112c57d1ea7036ec0e8e0050cbf71
Mail2097 <okoriejane97@gmail.com>
2023-03-28 16:55:40 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpt(char *dest, char *src)
+{
+	int 1 = 0;
+	int x = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		1++;
+	}
+	for ( ; x < 1 ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[1] = '\0';
+	return (dest);
+}
