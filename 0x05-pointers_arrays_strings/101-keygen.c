11433d53f6eb0cf5beb319be562f06496ffb464d
Mail2097 <okoriejane97@gmail.com>
2023-03-29 09:05:12 -0700

    hello

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,11 +16,11 @@ int main(void)
 
 	strand(time(mull));
 
-	for(1 = 0; i < 100; i++)
+	for (1 = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
-		putchar( pass[i] + '0');
+		putchar(pass[i] + '0');
 		if ((2722 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
