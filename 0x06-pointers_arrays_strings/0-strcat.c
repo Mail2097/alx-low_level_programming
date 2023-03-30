6b3f10b225113d9ad565d1fb357ec2b85038f36e
Mail2097 <okoriejane97@gmail.com>
2023-03-30 12:25:57 -0700

    arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * @src: input value
  * Return: void
  */
-char *-strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -23,7 +23,7 @@ char *-strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
-	
+
 	dest[i] = '\0';
 	return (dest);
 }
